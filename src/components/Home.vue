a3b4496e16d61fb9b0843e93119b01850520581b
Tadessejemal1 <tadessejemal63@gmail.com>
2024-07-05 16:00:25 +0300

    add vue.js function for Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <Header />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import Header from './Header.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  },
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        router.push({ name: 'SignUp' });
+      }
+    });
+  }
+};
+</script>
